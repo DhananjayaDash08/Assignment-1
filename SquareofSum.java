844240527288518a2cf2459d3340ef2d0c09c59a
DhananjayaDash08 <94070065+DhananjayaDash08@users.noreply.github.com>
2021-12-14 17:17:32 +0530

    Add files via upload

---
+++ SquareofSum.java
@@ -0,0 +1,15 @@
+package Geekster_Test;
+
+public class SquareofSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, sum=0, n=10;
+		for(i=1;i<=n;i++)
+		{
+			sum=sum+i*i;
+		}
+		System.out.print(sum);
+	}
+
+}
